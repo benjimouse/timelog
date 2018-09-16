5d4409f1b446cf5e12bad9ab105392fe88c0880a
Ben Best <ben.best@olmgroup.com>
2018-09-16 23:37:24 +0100

    Feat: And now it sort of works.

--- timelog.go
+++ timelog.go
@@ -21,48 +21,60 @@ type Person struct {
 	Name  string
 	Phone string
 }
+type Task struct {
+	Time  time.Time
+	Event string
+}
 
 func main() {
-	configuration := Configuration{}
-	gonfig.GetConf("config/config.development.json", &configuration)
-
-	mongoDBDialInfo := &mgo.DialInfo{
-		Addrs:    []string{configuration.MongoDBHost},
-		Timeout:  60 * time.Second,
-		Database: configuration.Database,
-		Username: configuration.AuthUserName,
-		Password: configuration.AuthPassword,
-	}
-
-	// Create a session which maintains a pool of socket connections
-	// to our MongoDB.
-	mongoSession, err := mgo.DialWithInfo(mongoDBDialInfo)
-	mongoSession.SetMode(mgo.Monotonic, true)
-
-	sessionCopy := mongoSession.Copy()
-	defer sessionCopy.Close()
-
-	c := sessionCopy.DB(configuration.Database).C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Phone:", result.Phone)
-
-	fmt.Println(configuration.MongoDBHost)
-	t := time.Now()
-	fmt.Println(t.Format(time.RFC3339))
+	// Really if you're not going to send in an argument then ...
 	if len(os.Args) != 1 {
 		a := os.Args[1] // The value passed into the command line
-		fmt.Println(a)
+
+		configuration := Configuration{}
+		gonfig.GetConf("config/config.development.json", &configuration)
+
+		mongoDBDialInfo := &mgo.DialInfo{
+			Addrs:    []string{configuration.MongoDBHost},
+			Timeout:  60 * time.Second,
+			Database: configuration.Database,
+			Username: configuration.AuthUserName,
+			Password: configuration.AuthPassword,
+		}
+
+		// Create a session which maintains a pool of socket connections
+		// to our MongoDB.
+		mongoSession, err := mgo.DialWithInfo(mongoDBDialInfo)
+		mongoSession.SetMode(mgo.Monotonic, true)
+
+		sessionCopy := mongoSession.Copy()
+		defer sessionCopy.Close()
+
+		c := sessionCopy.DB(configuration.Database).C("events")
+
+		err = c.Insert(&Task{time.Now(), a})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		result := Task{}
+		err = c.Find(bson.M{"event": a}).One(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Event:", result.Event)
+
+		fmt.Println(configuration.MongoDBHost)
+		t := time.Now()
+		fmt.Println(t.Format(time.RFC3339))
+		if len(os.Args) != 1 {
+			a := os.Args[1] // The value passed into the command line
+			fmt.Println("Orig", a)
+		}
+		fmt.Println("Creating a timelog.")
+
+	} else {
+		fmt.Println("No event data sent in")
 	}
-	fmt.Println("Creating a timelog.")
 }
