46d46587e5db8e1f39bc62151f6bbaf704e91675
Ben Best <ben.best@olmgroup.com>
2018-09-16 21:27:24 +0100

    Feat: Adding in some date bits.

--- timelog.go
+++ timelog.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"github.com/benjimouse/stringutil"
+)
 
 func main() {
+	t := time.Now()
+	fmt.Println(t.Year(), "-", int(t.Month()), "-", t.Day())
 	fmt.Println("Creating a timelog.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
